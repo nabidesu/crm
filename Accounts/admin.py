ec65dff761d19b892045bc03932c6c8a30028998
Apeksya Adhikari <adhikariapekxya299@gmail.com>
2025-01-05 11:47:21 +0545

    inigt

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer
+from .models import Customer, Positive_reviews, Negative_reviews
 admin.site.register(Customer)
+admin.site.register(Negative_reviews)
+admin.site.register(Positive_reviews)
--- Accounts/models.py
+++ Accounts/models.py
@@ -6,21 +6,19 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
-    data_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
 
 
-class reviews (models.Model):
+class Positive_reviews (models.Model):
     review = models.CharField(max_length=5000)
-    data_created = models.DateTimeField(auto_now_add=True, null=True)
-
-
-class customer_reviews(models.Model):
     STATUS = (('Pending', 'Pending'), ('Reviewed', 'Reviewed'))
 
     status = models.CharField(max_length=5000, choices=STATUS)
-    # customer=
-    # reviews=
-    data_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Negative_reviews(models.Model):
+    STATUS = (('Pending', 'Pending'), ('Reviewed', 'Reviewed'))
+
+    status = models.CharField(max_length=5000, choices=STATUS)
